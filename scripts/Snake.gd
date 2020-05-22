3cf08ac1b42cd6c876ea4acaad68b7dcf53eece9
arnath <prakash.vijay@gmail.com>
2020-05-22 21:46:22 +0200

    Finally got interpolated grid movement to work correctly.

--- scripts/Snake.gd
+++ scripts/Snake.gd
@@ -8,8 +8,8 @@ enum Direction {
     DOWN
 }
 
-export(int) var tile_size_pixels = 64
-export(float) var speed_tiles_per_sec = 0.1
+export(int) var tile_size_pixels = 16
+export(float) var speed_tiles_per_sec = 20
 
 var movementVectors = {
     0: Vector2.RIGHT,
@@ -18,12 +18,10 @@ var movementVectors = {
     3: Vector2.DOWN
 }
 
-var direction := -1
-var screen_size: Vector2
-
-func _ready() -> void:
-    screen_size = get_viewport_rect().size
-    hide()
+var direction = -1
+var originalPosition: Vector2
+var targetPosition: Vector2
+var offset: float = 0
 
 # Set movement direction based on unhandled input.
 func _unhandled_input(_event: InputEvent) -> void:
@@ -35,14 +33,23 @@ func _unhandled_input(_event: InputEvent) -> void:
         direction = 2
     elif Input.is_action_pressed("ui_down") and direction != 2:
         direction = 3
-            
-func _physics_process(_delta: float) -> void:
-    if direction != -1:
-        position += movementVectors[direction] * tile_size_pixels * speed_tiles_per_sec
-        position.x = clamp(position.x, 0, screen_size.x)
-        position.y = clamp(position.y, 0, screen_size.y)
+
+func _physics_process(delta: float) -> void:
+    var weight = delta * speed_tiles_per_sec
+    
+    # If the next cell has been reached, set a new target.
+    if position == targetPosition and direction != -1:
+        originalPosition = position
+        targetPosition = position + (movementVectors[direction] * tile_size_pixels)
+        offset = 0
+        
+    # Move toward the target.
+    if position != targetPosition:
+        offset += weight
+        position = lerp(originalPosition, targetPosition, offset)
     
 # Start a new game.
 func start(pos: Vector2) -> void:
     position = pos
+    targetPosition = pos
     show()
