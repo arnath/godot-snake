c43dbc2057fb4775bfdc36620dc91c18a3dce348
arnath <prakash.vijay@gmail.com>
2020-05-29 23:43:40 +0200

    Added score tracking.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,11 +2,14 @@ extends Node
 
 export(PackedScene) var Food
 
+var score: int
+
 func _ready() -> void:
     randomize()
     new_game()
 
 func new_game() -> void:
+    _set_score(0)
     $Snake.start($StartPosition.position)
     _spawn_food()
     
@@ -21,3 +24,11 @@ func _spawn_food() -> void:
     var food: Food = Food.instance()
     food.position = food_position
     add_child(food)
+    
+func _set_score(new_score: int) -> void:
+    score = new_score
+    $ScoreNumber.text = str(score)
+
+func _on_Snake_ate_food() -> void:
+    _set_score(score + 1)
+    _spawn_food()
